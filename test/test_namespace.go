125416f50af79bc95264b2b3b0285bc3f0d48b89
superniaoren <superniaoren@yahoo.com>
2018-12-27 07:59:34 +0800

    user+network namespace

--- test/test_namespace.go
+++ test/test_namespace.go
@@ -14,11 +14,22 @@ func main(){
     cmd := exec.Command("sh")
     cmd.SysProcAttr = &syscall.SysProcAttr{ 
             //Cloneflags: syscall.CLONE_NEWUTS,
+            // new ipc
             //Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC,
+            // new pid
             //Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID,
             // mount namespace
-            Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+            //Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+            // new user
+            //Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+            // new network
+            Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | 
+                         syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
     }
+    // root user ?; must work with CLONE_NEWUSER
+    // fork/exec /bin/sh: operation not permitted
+    //cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
@@ -26,4 +37,5 @@ func main(){
     if err:= cmd.Run(); err != nil {
         log.Fatal(err)
     }
+    os.Exit(-1)
 }
